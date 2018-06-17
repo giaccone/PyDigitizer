0e95b0c75f21e64ef74857cf62170a3a4fed5c3c
Luca Giaccone <luca.giaccone@polito.it>
2018-06-17 15:37:43 +0200

    more control tools are initialized

--- PyDigitizer.py
+++ PyDigitizer.py
@@ -1,6 +1,8 @@
 # modules
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout
 from PyQt5.QtWidgets import QGroupBox, QPushButton, QFileDialog, QSizePolicy
+from PyQt5.QtWidgets import QRadioButton
+from PyQt5.QtCore import Qt
 #
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
@@ -69,36 +71,110 @@ class MainWidget(QWidget):
 
     def __init__(self, parent):
         super().__init__()
+
+        self.Xpic_min = None
+        self.Xpic_max = None
+        self.Ypic_min = None
+        self.Ypic_max = None
+
+        self.XScaleType = 'linear'
+        self.YScaleType = 'linear'
+
         self.initUI()
 
 
     def initUI(self):
 
         # First Column (Commands)
-        VBox1 = QGroupBox()
-        Layout1 = QGridLayout()
+        VBoxSx1 = QGroupBox()
+        LayoutSx1 = QGridLayout()
         #
         LoadFileButton = QPushButton('Load Image',self)
         LoadFileButton.clicked.connect(self.loadImage)
         #
-        Layout1.addWidget(LoadFileButton,0,0)
-        VBox1.setLayout(Layout1)
+        LayoutSx1.addWidget(LoadFileButton,0,0)
+        VBoxSx1.setLayout(LayoutSx1)
 
-        # First Column (Figure)
-        VBox2 = QGroupBox()
+        VBoxSx2 = QGroupBox()
+        VBoxSx2.setTitle('Picture limits')
+        LayoutSx2 = QGridLayout()
+        #
+        XminButton = QPushButton('Pick X_min',self)
+        YminButton = QPushButton('Pick Y_min',self)
+        XmaxButton = QPushButton('Pick X_max',self)
+        YmaxButton = QPushButton('Pick Y_max',self)
+        #
+        LayoutSx2.addWidget(XminButton,0,0)
+        LayoutSx2.addWidget(YminButton,1,0)
+        LayoutSx2.addWidget(XmaxButton,2,0)
+        LayoutSx2.addWidget(YmaxButton,3,0)
+        #
+        XminButton.clicked.connect(self.pickXmin)
+        YminButton.clicked.connect(self.pickYmin)
+        XmaxButton.clicked.connect(self.pickXmax)
+        YmaxButton.clicked.connect(self.pickYmax)
+        #
+        VBoxSx2.setLayout(LayoutSx2)
+
+        VBoxSx3 = QGroupBox()
+        VBoxSx3.setTitle('x scale')
+        LayoutSx3 = QGridLayout()
+        self.Xlinear = QRadioButton('linear')
+        self.Xlinear.setChecked(True)
+        self.Xlog = QRadioButton('log')
+        LayoutSx3.addWidget(self.Xlinear,0,0)
+        LayoutSx3.addWidget(self.Xlog,0,1)
+        #
+        self.Xlinear.clicked.connect(self.setXScaleType)
+        self.Xlog.clicked.connect(self.setXScaleType)
+        #
+        VBoxSx3.setLayout(LayoutSx3)
+
+        VBoxSx4 = QGroupBox()
+        VBoxSx4.setTitle('y scale')
+        LayoutSx4 = QGridLayout()
+        self.Ylinear = QRadioButton('linear')
+        self.Ylinear.setChecked(True)
+        self.Ylog = QRadioButton('log')
+        LayoutSx4.addWidget(self.Ylinear,0,0)
+        LayoutSx4.addWidget(self.Ylog,0,1)
+        #
+        self.Ylinear.clicked.connect(self.setYScaleType)
+        self.Ylog.clicked.connect(self.setYScaleType)
+        #
+        VBoxSx4.setLayout(LayoutSx4)
+
+
+
+        VBoxSx5 = QGroupBox()
+        LayoutSx5 = QGridLayout()
+
+        PickPointButton = QPushButton('Pick Point',self)
+        PickPointButton.clicked.connect(self.pickPoints)
+
+        LayoutSx5.addWidget(PickPointButton,1,0)
+        VBoxSx5.setLayout(LayoutSx5)
+
+        # Second Column (Figure)
+        VBoxDx1 = QGroupBox()
         Layout2 = QGridLayout()
         #
         self.FigCanvas = MplCanvas(self, width=5, height=4, dpi=100)
-        toolbar = NavigationToolbar(self.FigCanvas, VBox2)
+        toolbar = NavigationToolbar(self.FigCanvas, VBoxDx1)
         #
         Layout2.addWidget(self.FigCanvas,0,0)
         Layout2.addWidget(toolbar,1,0)
-        VBox2.setLayout(Layout2)
+        VBoxDx1.setLayout(Layout2)
 
         # Compose Windows
         windowLayout = QGridLayout()
-        windowLayout.addWidget(VBox1, 0, 0)
-        windowLayout.addWidget(VBox2, 0, 1)
+        windowLayout.addWidget(VBoxSx1, 0, 0)
+        windowLayout.addWidget(VBoxSx2, 1, 0)
+        windowLayout.addWidget(VBoxSx3, 2, 0)
+        windowLayout.addWidget(VBoxSx4, 3, 0)
+        windowLayout.addWidget(VBoxSx5, 4, 0)
+        #
+        windowLayout.addWidget(VBoxDx1, 0, 1, 5, 1)
         # Stretches
         windowLayout.setColumnStretch(0, 0)
         windowLayout.setColumnStretch(1, 2)
@@ -108,12 +184,79 @@ class MainWidget(QWidget):
 
 
     def loadImage(self):
+
         filename, _ = QFileDialog.getOpenFileName()
         img = plt.imread(filename)
         self.FigCanvas.axes.clear()
         self.FigCanvas.axes.imshow(img)
         self.FigCanvas.axes.axis('off')
         self.FigCanvas.draw_idle()
+
+
+    def pickXmin(self):
+
+        self.FigCanvas.setFocusPolicy( Qt.ClickFocus )
+        self.FigCanvas.setFocus()
+        pt = self.FigCanvas.figure.ginput(n=1)
+        self.Xpic_min = pt[0][0]
+
+        print(self.Xpic_min)
+
+
+    def pickYmin(self):
+
+        self.FigCanvas.setFocusPolicy( Qt.ClickFocus )
+        self.FigCanvas.setFocus()
+        pt = self.FigCanvas.figure.ginput(n=1)
+        self.Ypic_min = pt[0][1]
+
+        print(self.Ypic_min)
+
+
+    def pickXmax(self):
+
+        self.FigCanvas.setFocusPolicy( Qt.ClickFocus )
+        self.FigCanvas.setFocus()
+        pt = self.FigCanvas.figure.ginput(n=1)
+        self.Xpic_max = pt[0][0]
+
+        print(self.Xpic_max)
+
+    def pickYmax(self):
+
+        self.FigCanvas.setFocusPolicy( Qt.ClickFocus )
+        self.FigCanvas.setFocus()
+        pt = self.FigCanvas.figure.ginput(n=1)
+        self.Ypic_max = pt[0][1]
+
+        print(self.Ypic_max)
+
+    def setXScaleType(self):
+        if self.Xlinear.isChecked():
+            self.XScaleType = 'linear'
+        elif self.Xlog.isChecked():
+            self.XScaleType = 'log'
+
+        print('Xscale is ' + self.XScaleType)
+
+    def setYScaleType(self):
+        if self.Ylinear.isChecked():
+            self.YScaleType = 'linear'
+        elif self.Ylog.isChecked():
+            self.YScaleType = 'log'
+
+        print('Yscale is ' + self.YScaleType)
+
+
+
+    def pickPoints(self):
+
+        self.FigCanvas.setFocusPolicy( Qt.ClickFocus )
+        self.FigCanvas.setFocus()
+        pt = self.FigCanvas.figure.ginput(n=-1)
+
+        print(pt)
+
 
 
 # Start App
